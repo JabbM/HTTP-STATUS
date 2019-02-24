a997c18334624e70a9a703d97f91581a55abb08d
JabbM <jullian.bacle@ynov.com>
2019-02-24 15:02:12 +0100

    try again

--- httpstatus/templates/home.php
+++ httpstatus/templates/home.php
@@ -7,7 +7,7 @@ require PWD_TEMPLATES . '/header.php';
 
 
 
-      <div class="starter-template mt-5">
+      <div class="starter-template mt-5 pt-5">
         <h1>BIENVENUE</h1>
         <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem fuga reiciendis blanditiis? Voluptas nesciunt nostrum delectus expedita, quaerat sequi reprehenderit. Impedit et laudantium non ipsa excepturi consectetur fugit, repellat illum?</p>
       </div>
